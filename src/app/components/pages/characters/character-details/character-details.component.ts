e0e6f696310f45c38fff2ff03e2890ce8d4d3819
carloscruz20 <carlosdv92@gmail.com>
2020-07-14 12:24:26 -0500

    v2

---
+++ src/app/components/pages/characters/character-details/character-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Character } from '@app/shared/components/interfaces/character.interface';
+import { ActivatedRoute } from '@angular/router';
+import { CharacterService } from '@app/shared/services/character.service';
+import { Location } from "@angular/common";
+import { take } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-character-details',
+  templateUrl: './character-details.component.html',
+  styleUrls: ['./character-details.component.scss']
+})
+export class CharacterDetailsComponent implements OnInit {
+  
+  character$:Observable<Character>;
+
+  constructor(private route:ActivatedRoute,private characterSvc:CharacterService,
+              private location:Location) { }
+
+  ngOnInit(): void {
+    this.route.params.pipe(take(1)).subscribe((params)=>{
+      const id= params['id'];
+      this.character$=this.characterSvc.getDetails(id);
+    })
+  }
+
+  ongoBack():void{
+    this.location.back();
+  }
+
+}
